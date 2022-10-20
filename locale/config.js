af75db4533756ea75053d1e8d2ad7ba44eaad966
Robert Korulczyk <robert@korulczyk.pl>
2022-10-20 22:46:52 +0200

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,61 +1,6 @@
-function relativeTimeWithTense(number, withoutSuffix, key, isFuture) {
-    const format = {
-        s: ['mõne sekundi', 'mõni sekund', 'paar sekundit'],
-        m: ['ühe minuti', 'üks minut'],
-        mm: ['%d minuti', '%d minutit'],
-        h: ['ühe tunni', 'tund aega', 'üks tund'],
-        hh: ['%d tunni', '%d tundi'],
-        d: ['ühe päeva', 'üks päev'],
-        M: ['kuu aja', 'kuu aega', 'üks kuu'],
-        MM: ['%d kuu', '%d kuud'],
-        y: ['ühe aasta', 'aasta', 'üks aasta'],
-        yy: ['%d aasta', '%d aastat']
-    }
-    if (withoutSuffix) {
-        return (format[key][2] ? format[key][2] : format[key][1]).replace('%d', number)
-    }
-    return (isFuture ? format[key][0] : format[key][1]).replace('%d', number)
-}
+// source: https://unpkg.com/dayjs@v1.10.4/locale/et.js
+// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/et.js
 
-dayjs.locale({
-    name: 'et',
-    weekdays: 'pühapäev_esmaspäev_teisipäev_kolmapäev_neljapäev_reede_laupäev'.split('_'),
-    weekdaysShort: 'P_E_T_K_N_R_L'.split('_'),
-    weekdaysMin: 'P_E_T_K_N_R_L'.split('_'),
-    months: 'jaanuar_veebruar_märts_aprill_mai_juuni_juuli_august_september_oktoober_november_detsember'.split('_'),
-    monthsShort: 'jaan_veebr_märts_apr_mai_juuni_juuli_aug_sept_okt_nov_dets'.split('_'),
-    ordinal: n => `${n}.`,
-    weekStart: 1,
-    yearStart: 4,
-    relativeTime: {
-        future: '%s pärast',
-        past: '%s tagasi',
-        s: relativeTimeWithTense,
-        m: relativeTimeWithTense,
-        mm: relativeTimeWithTense,
-        h: relativeTimeWithTense,
-        hh: relativeTimeWithTense,
-        d: relativeTimeWithTense,
-        dd: '%d päeva',
-        M: relativeTimeWithTense,
-        MM: relativeTimeWithTense,
-        y: relativeTimeWithTense,
-        yy: relativeTimeWithTense
-    },
-    formats: {
-        LT: 'H:mm',
-        LTS: 'H:mm:ss',
-        L: 'DD.MM.YYYY',
-        LL: 'D. MMMM YYYY',
-        LLL: 'D. MMMM YYYY H:mm',
-        LLLL: 'dddd, D. MMMM YYYY H:mm'
-    },
-    calendar: {
-        sameDay: '[täna] LT',
-        nextDay: '[homme] LT',
-        nextWeek: 'dddd LT',
-        lastDay: '[eile] LT',
-        lastWeek: '[eelmine] dddd LT',
-        sameElse: 'L'
-    },
-}, null, false);
+!function(e,a){"object"==typeof exports&&"undefined"!=typeof module?module.exports=a(require("dayjs")):"function"==typeof define&&define.amd?define(["dayjs"],a):e.dayjs_locale_et=a(e.dayjs)}(this,function(e){"use strict";function a(e,a,t,u){var s={s:["mõne sekundi","mõni sekund","paar sekundit"],m:["ühe minuti","üks minut"],mm:["%d minuti","%d minutit"],h:["ühe tunni","tund aega","üks tund"],hh:["%d tunni","%d tundi"],d:["ühe päeva","üks päev"],M:["kuu aja","kuu aega","üks kuu"],MM:["%d kuu","%d kuud"],y:["ühe aasta","aasta","üks aasta"],yy:["%d aasta","%d aastat"]};return a?(s[t][2]?s[t][2]:s[t][1]).replace("%d",e):(u?s[t][0]:s[t][1]).replace("%d",e)}e=e&&e.hasOwnProperty("default")?e.default:e;var t={name:"et",weekdays:"pühapäev_esmaspäev_teisipäev_kolmapäev_neljapäev_reede_laupäev".split("_"),weekdaysShort:"P_E_T_K_N_R_L".split("_"),weekdaysMin:"P_E_T_K_N_R_L".split("_"),months:"jaanuar_veebruar_märts_aprill_mai_juuni_juuli_august_september_oktoober_november_detsember".split("_"),monthsShort:"jaan_veebr_märts_apr_mai_juuni_juuli_aug_sept_okt_nov_dets".split("_"),ordinal:function(e){return e+"."},weekStart:1,relativeTime:{future:"%s pärast",past:"%s tagasi",s:a,m:a,mm:a,h:a,hh:a,d:a,dd:"%d päeva",M:a,MM:a,y:a,yy:a},formats:{LT:"H:mm",LTS:"H:mm:ss",L:"DD.MM.YYYY",LL:"D. MMMM YYYY",LLL:"D. MMMM YYYY H:mm",LLLL:"dddd, D. MMMM YYYY H:mm"}};return e.locale(t,null,!0),t});
+
+dayjs.locale('et');
